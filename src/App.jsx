c298967468eda5cabf782f269272f249a7509ea5
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-11 18:19:00 -0300

    maquetado inicial de página de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Inicio from './components/pages/Inicio'
 
 function App() {
   return (
     <>
-      <h1>Hola mundo !</h1>
+      <Inicio />
     </>
   )
 }
---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,18 @@
+import { Container, Row } from "react-bootstrap"
+
+
+const Inicio = () => {
+  return (
+    <main className="mainPage">
+      <img className="banner" src="https://images.unsplash.com/photo-1635321593217-40050ad13c74?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <Container className="mt-3">
+        <h1 className="display-3 text-center">RollingCook, tu blog de recetas</h1>
+        <Row>
+          {/* componente Card Receta */}
+        </Row>
+      </Container>
+    </main>
+  )
+}
+
+export default Inicio
