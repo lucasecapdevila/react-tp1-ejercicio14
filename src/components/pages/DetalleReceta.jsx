ec4bd7c8a8271ab13b0059e73b31d53ed7405f47
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-11 19:28:03 -0300

    se agregan ingredientes de la receta

--- src/components/pages/DetalleReceta.jsx
+++ src/components/pages/DetalleReceta.jsx
@@ -1,4 +1,3 @@
-import { Card, CardImg, CardText, CardTitle } from "react-bootstrap"
 
 const DetalleReceta = () => {
   return (
@@ -7,6 +6,16 @@ const DetalleReceta = () => {
       <div className="d-flex justify-content-center">
         <img className="img-fluid" src="https://1.bp.blogspot.com/-0knKqJDJPqo/VhZqXdXIpHI/AAAAAAAAPLs/_78vNUgpN60/s1600/tapa-de-asado-princ-2.jpg" alt="tapa de asado con verduras" />
       </div>
+      <h3 className="text-center my-3">Ingredientes (para 3 porciones)</h3>
+      <ul>
+        <li>Tapa de asado: 800g</li>
+        <li>Zanahorias baby o zanahorias comunes: 150g</li>
+        <li>Cebolla: 100g</li>
+        <li>Batata: 150g</li>
+        <li>Bolsa de horno Knorr Romero y Tomillo: 1</li>
+        <li>Aceite: 1 cucharadita</li>
+        <li>Azúcar negra: 1 cucharadita</li>
+      </ul>
     </main>
   )
 }
