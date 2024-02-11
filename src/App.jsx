6bf0c1d5f86409309c45db2949e734a8b5824266
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-11 20:39:03 -0300

    se agrega maquetado de página de Error 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import DetalleReceta from './components/pages/DetalleReceta'
+import Error404 from './components/pages/Error404'
 import Inicio from './components/pages/Inicio'
 
 function App() {
   return (
     <>
       {/* <Inicio /> */}
-      <DetalleReceta />
+      {/* <DetalleReceta /> */}
+      <Error404 />
     </>
   )
 }
---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,14 @@
+import { Button } from 'react-bootstrap'
+import error404 from '../../assets/404.png'
+
+const Error404 = () => {
+  return (
+    <main className="d-flex flex-column align-items-center my-4 mainPage">
+      <h1 className='text-center'>Lo sentimos, no pudimos encontrar la página</h1>
+      <img className='img-fluid my-2' src={error404} alt="" />
+      <Button>Volver al inicio</Button>
+    </main>
+  )
+}
+
+export default Error404
