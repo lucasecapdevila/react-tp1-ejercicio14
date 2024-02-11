c44f323bdb1a94c1c406dd6a4071b2d3653f8521
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-11 19:52:50 -0300

    se agrega el paso a paso para preparar la receta

--- src/components/pages/DetalleReceta.jsx
+++ src/components/pages/DetalleReceta.jsx
@@ -16,6 +16,44 @@ const DetalleReceta = () => {
         <li>Aceite: 1 cucharadita</li>
         <li>Azúcar negra: 1 cucharadita</li>
       </ul>
+
+      <h3 className="text-center my-3">Paso a paso</h3>
+      <ol>
+        <li className="my-4 ms-3">
+          Cortar las verduras y desgrasar la carne.
+        </li>
+        <div className="d-flex justify-content-center">
+          <img className="img-fluid" src="https://4.bp.blogspot.com/-ws1B6JQ1IM4/VhZqRTjNwKI/AAAAAAAAPLA/5S-0-Hox4rE/s1600/Tapa-de-asado-paso-1.jpg" alt="verduras picadas y carne desgrasada" />
+        </div>
+
+        <li className="my-4 ms-3">
+          Rociar las verduras con el aceite y el azúcar ayudándose con las manos para que se impregnen de esta manera las verduras van a quedar con un toque agridulce.
+        </li>
+        <div className="d-flex justify-content-center">
+          <img className="img-fluid" src="https://4.bp.blogspot.com/-9SZozGsMf_w/VhZqSgLy2JI/AAAAAAAAPLM/q6usyTgBrOk/s1600/tapa-de-asado-paso-2.jpg" alt="verduras sazonadas" />
+        </div>
+
+        <li className="my-4 ms-3">
+          Abrir la bolsa de horno y ponerla sobre una asadera. Colocar la carne y las verduras, y esparcir el condimento ayudándose con las manos. Cerrar la bolsa.
+        </li>
+        <div className="d-flex justify-content-center">
+          <img className="img-fluid" src="https://4.bp.blogspot.com/-c7Hwkm8HqsU/VhZqT-v6frI/AAAAAAAAPLU/7SzjdEF-XBY/s1600/tapa-de-asado-paso3.jpg" alt="carne y verduras en la bolsa para horno" />
+        </div>
+
+        <li className="my-4 ms-3">
+          Llevar a horno 180° y cocinar por 60/70 minutos (mientras se cocina tenés una hora para hacer lo que quieras!). Cortar la bolsa con cuidado porque sale mucho vapor, servir.
+        </li>
+        <div className="d-flex justify-content-center">
+          <img className="img-fluid" src="https://3.bp.blogspot.com/-Rl0edLDtcGo/VhZqVTRHLwI/AAAAAAAAPLc/DpmQAdzm3rQ/s1600/tapa-de-asado-paso-4.jpg" alt="carne y verduras horneadas" />
+        </div>
+
+        <li className="my-4 ms-3">
+          A comer!!
+        </li>
+        <div className="d-flex justify-content-center">
+          <img className="img-fluid" src="https://4.bp.blogspot.com/-ps_LZOz-RIY/VhZqWVqlfYI/AAAAAAAAPLk/gG_1bNiwVxY/s1600/tapa-de-asado-princ-1.jpg" alt="Tapa de asado con verduras al horno" />
+        </div>
+      </ol>
     </main>
   )
 }
