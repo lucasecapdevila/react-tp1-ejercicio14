466c5d65bb80ee76cc140a0724ed04d84598267b
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-14 20:16:55 -0300

    se completa formulario para crear una receta

--- src/components/pages/receta/FormularioReceta.jsx
+++ src/components/pages/receta/FormularioReceta.jsx
@@ -1,29 +1,56 @@
 import { Button, Form } from "react-bootstrap"
+import { useForm } from "react-hook-form"
 
 const FormularioReceta = () => {
+  const {register, handleSubmit, formState: {errors}} = useForm()
+
+  const recetaValida = (receta) => {
+    console.log(receta);
+  }
+
   return (
     <section className="container mainPage">
       <h1 className="display-3 mt-5">Nueva Receta</h1>
       <hr />
-      <Form className="my-4">
+      <Form onSubmit={handleSubmit(recetaValida)} className="my-4">
         <Form.Group className="mb-3" controlId="formNombreReceta">
           <Form.Label>Nombre de receta<span className="text-danger">*</span></Form.Label>
           <Form.Control
             type="text"
-            placeholder="Ej: Cafe"
-            required
+            placeholder="Ej: Pollo al horno"
+            {
+              ...register("nombreReceta", {
+                required: 'El nombre de la receta es obligatorio.',
+                minLength:{
+                  value: 2,
+                  message: 'Debe ingresar como mínimo 2 carácteres para el nombre de la receta.'
+                },
+                maxLength:{
+                  value: 50,
+                  message: 'Debe ingresar como máximo 50 carácteres para el nombre de la receta.'
+                },
+              })
+            }
           />
-          <Form.Text className="text-danger">mensaje de error{/* {errors.nombreProducto?.message} */}</Form.Text>
+          <Form.Text className="text-danger">{errors.nombreReceta?.message}</Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL<span className="text-danger">*</span></Form.Label>
           <Form.Control
             type="text"
-            placeholder="URL de imagen de la receta"
-            required
+            placeholder="Ej: https://www.annarecetasfaciles.com/files/contramusloa-pollo-salsa-cebolla-3-815x458.jpg"
+            {
+              ...register("imagen", {
+                required: 'La URL de la imagen de la receta es obligatoria.',
+                pattern:{
+                  value: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/,
+                  message: 'La URL ingresada debe ser una imagen.'
+                }
+              })
+            }
           />
-          <Form.Text className="text-danger">mensaje de error</Form.Text>
+          <Form.Text className="text-danger">{errors.imagen?.message}</Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formCategoria">
@@ -43,13 +70,26 @@ const FormularioReceta = () => {
           <Form.Label>Descripción<span className="text-danger">*</span></Form.Label>
           <Form.Control
             type="text"
-            placeholder="Descripción de la receta"
-            required
+            placeholder="Ej: Un exquisito plato con atrapante aroma."
+            as="textarea"
+            {
+              ...register("descripcion", {
+                required: 'La descripción de la receta es obligatoria.',
+                minLength:{
+                  value: 10,
+                  message: 'Debe ingresar como mínimo 10 carácteres para la descripción de la receta.'
+                },
+                maxLength:{
+                  value: 200,
+                  message: 'Debe ingresar como máximo 200 carácteres para la descripción de la receta.'
+                }
+              })
+            }
           />
-          <Form.Text className="text-danger">mensaje de error</Form.Text>
+          <Form.Text className="text-danger">{errors.descripcion?.message}</Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formIngredientes">
+        {/* <Form.Group className="mb-3" controlId="formIngredientes">
           <Form.Label>Ingredientes<span className="text-danger">*</span></Form.Label>
           <Form.Control
             type="text"
@@ -67,7 +107,7 @@ const FormularioReceta = () => {
             required
           />
           <Form.Text className="text-danger">mensaje de error</Form.Text>
-        </Form.Group>
+        </Form.Group> */}
 
         <Button type="submit" variant="success">Guardar</Button>
       </Form>
